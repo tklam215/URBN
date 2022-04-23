a5cec0028d8defcd6f5df92c9a0d58030840b033
tklam215 <lam.tsunkit215@gmail.com>
2022-04-23 17:14:09 -0400

    changed celsius to Fahrenheit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [data,setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&unit=imperial&appid=add24a5088aed081dd1adb7368be93d2`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=add24a5088aed081dd1adb7368be93d2`
 
   const searchLocation = (event) =>{
     if(event.key == 'Enter'){
